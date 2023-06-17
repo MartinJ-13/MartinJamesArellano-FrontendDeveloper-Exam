1a3a34f18ce6b40c7c30452009af51b5dd0b66c9
MartinJ-13 <martin_arellano@dlsu.edu.ph>
2023-06-17 18:19:49 +0800

    Initial CSS style set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './index.css';
+import DisplayCard from './components/displaycard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>News Articles</h2>
+        <div className="checkbox-div">
+          <input type="checkbox" className="rounded-checkbox"/>
+        </div>
+        <button className="publish">Publish</button>
+        <button className="delete">Delete</button>
+      </div>
+      <DisplayCard text="fringilla"></DisplayCard>
+      ball
     </div>
   );
 }
---
+++ src/components/displaycard.js
@@ -0,0 +1,14 @@
+function DisplayCard(props){
+
+    return (
+      <>
+        <div className="display-card">
+          <h3>{props.text}</h3>
+        </div>
+      </>
+    );
+
+
+ }
+
+export default DisplayCard;
